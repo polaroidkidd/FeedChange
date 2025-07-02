0531e4974bc877129806855278adb1385435acb5
Daniel Einars <contact@dle.dev>
2025-07-02 22:46:47 +0200

    make it work again

--- src/lib/server/db/db.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '@prisma/client';
-
-const adapter = new PrismaBetterSQLite3({
-	url: 'file:./prisma/dev.db'
-});
-const prisma = new PrismaClient({ adapter });
